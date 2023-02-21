060c861d5ad5e10955de596144377a769aceb36f
Tomasz Ostrozanski <tomasz.ostrozanski@westrock.com>
2023-02-21 13:57:40 +0100

    Stage 4

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
